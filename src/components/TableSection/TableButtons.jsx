267fc570586c74504a93bcaeeb1e9f93badce5e2
John Mendez <john.manuel.mendez@gmail.com>
2020-06-23 17:48:40 -0400

    Style Table buttons

--- src/components/TableSection/TableButtons.jsx
+++ src/components/TableSection/TableButtons.jsx
@@ -1,12 +1,14 @@
-import { Button } from '@blueprintjs/core'
+import { Button, ButtonGroup } from '@blueprintjs/core'
+import { IconNames } from '@blueprintjs/icons'
 import { styled } from 'linaria/react'
 import React from 'react'
 
 const ButtonRow = styled.div`
-	float: right;
+	display: flex;
+	justify-content: flex-end;
 	margin-bottom: 40px;
 `
-const StyledCodeSnippet = styled(Button)`
+const StyledCodeSnippet = styled(ButtonGroup)`
 	margin: 0px 16px;
 `
 
@@ -15,11 +17,20 @@ const S = {
 	CodeSnippet: StyledCodeSnippet,
 }
 
-const SaveAsList = () => <Button text="Save As List" />
+const SaveAsList = () => (
+	<Button outlined={true} intent="primary" icon={IconNames.CLOUD_UPLOAD} text="Save As List" />
+)
 
-const CodeSnippet = () => <S.CodeSnippet text="code snippet" />
+const CodeSnippet = () => (
+	<S.CodeSnippet>
+		<Button outlined={true} intent="primary" icon={IconNames.CODE} text="code snippet" />
+		<Button outlined={true} intent="primary" icon={IconNames.CARET_DOWN} />
+	</S.CodeSnippet>
+)
 
-const Export = () => <Button text="Export" />
+const Export = () => (
+	<Button intent="primary" outlined={true} icon={IconNames.ARCHIVE} text="Export" />
+)
 
 export const TableButtons = () => {
 	return (
