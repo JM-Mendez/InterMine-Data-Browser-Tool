f1211e815a150babd3312869a786f714d6ce8ade
John Mendez <john.manuel.mendez@gmail.com>
2020-06-23 16:13:19 -0400

    Render row cells

--- src/components/Layout/TableSection.jsx
+++ src/components/Layout/TableSection.jsx
@@ -1,6 +1,5 @@
 import { Card, Classes, HTMLTable, Icon } from '@blueprintjs/core'
 import { IconNames } from '@blueprintjs/icons'
-import { Cell, Column, Table } from '@blueprintjs/table'
 import { css } from 'linaria'
 import { styled } from 'linaria/react'
 import React from 'react'
@@ -8,49 +7,33 @@ import { humanize, titleize } from 'underscore.string'
 
 import { humanMine25 as rows } from '../../stubs/humanMine25'
 
+const StyledTable = styled(HTMLTable)`
+	width: 100%;
+`
+
 const TableCard = styled(Card)`
 	height: 500px;
 	margin-bottom: 20px;
+	overflow: scroll;
 `
 
-const StyledCell = styled(Cell)`
-	font-size: var(--fs-desktopS2);
-	display: flex;
-	align-items: center;
+const StyledCell = styled.td`
+	&& a {
+		font-weight: normal;
 
-	& .${Classes.ICON} {
-		padding-right: 5px;
+		& .${Classes.ICON} {
+			padding-right: 5px;
+		}
 	}
 `
-
-const StyledTable = styled(Table)``
 
 const S = {
 	TableCard,
 	Cell: StyledCell,
+	Table: StyledTable,
 }
 
 const mineUrl = 'https://www.humanmine.org/humanmine'
-
-const cellRenderer = (rowIndex, columnIndex) => {
-	const cell = rows[rowIndex][columnIndex]
-	const cellValue = cell.value
-
-	return (
-		<S.Cell key={`${rowIndex}${columnIndex}`} wrapText={true}>
-			{cellValue ? (
-				// disable this rule since we do want to know who the referrer is
-				// eslint-disable-next-line react/jsx-no-target-blank
-				<a href={`${mineUrl}${cell.url}`} target="_blank">
-					<Icon icon={IconNames.GLOBE_NETWORK} />
-					{cellValue}
-				</a>
-			) : (
-				<span className={Classes.TEXT_DISABLED}>No Value</span>
-			)}
-		</S.Cell>
-	)
-}
 
 const ColumnHeader = ({ columnName }) => {
 	const name = titleize(humanize(columnName.replace(/\./g, ' ')))
@@ -72,15 +55,32 @@ const ColumnHeader = ({ columnName }) => {
 	)
 }
 
+const Cell = ({ cell }) => {
+	const cellValue = cell.value
+
+	return (
+		<S.Cell scope="row">
+			{cellValue ? (
+				<div title={cellValue}>
+					{/* disable this rule since we do want to know who the referrer is */}
+					{/* eslint-disable-next-line react/jsx-no-target-blank */}
+					<a href={`${mineUrl}${cell.url}`} target="_blank">
+						<Icon icon={IconNames.GLOBE_NETWORK} />
+						{cellValue}
+					</a>
+				</div>
+			) : (
+				<span className={Classes.TEXT_DISABLED}>No Value</span>
+			)}
+		</S.Cell>
+	)
+}
+
 export const TableSection = () => {
 	return (
 		<section>
 			<S.TableCard>
-				<HTMLTable
-					className={css`
-						width: 100%;
-					`}
-				>
+				<S.Table interactive={true} striped={true}>
 					<thead>
 						<tr>
 							{rows[0].map((r) => {
@@ -88,7 +88,18 @@ export const TableSection = () => {
 							})}
 						</tr>
 					</thead>
-				</HTMLTable>
+					<tbody>
+						{rows.map((row) => {
+							return (
+								<tr>
+									{row.map((cell) => (
+										<Cell cell={cell} />
+									))}
+								</tr>
+							)
+						})}
+					</tbody>
+				</S.Table>
 			</S.TableCard>
 		</section>
 	)
