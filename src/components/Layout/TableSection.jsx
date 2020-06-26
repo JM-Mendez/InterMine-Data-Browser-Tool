c2e4a7f7b5a738ac059292a19d7f4cd9cd7d6b2b
John Mendez <john.manuel.mendez@gmail.com>
2020-06-26 08:44:54 -0400

    Move Table component to DataViz dir

--- src/components/Layout/TableSection.jsx
+++ src/components/Layout/TableSection.jsx
@@ -4,8 +4,7 @@ import React from 'react'
 
 import { humanMine25 as rows } from '../../stubs/humanMine25'
 import { mineUrl } from '../../stubs/utils'
-import { Table } from '../Table/Table'
-import { TableActionButtons, TablePagingButtons } from '../Table/TableButtons'
+import { Table, TableActionButtons, TablePagingButtons } from '../DataViz'
 
 const TableCard = styled(Card)`
 	margin-bottom: 20px;
