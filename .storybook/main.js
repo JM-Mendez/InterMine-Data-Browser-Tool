af4e03ff42c51ba90cf2ae825acd15dc3c109775
John Mendez <john.manuel.mendez@gmail.com>
2020-06-25 22:29:20 -0400

    Deploy only the icons we use to production

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,7 @@
+const { NormalModuleReplacementPlugin } = require('webpack')
+const { extractIcons } = require('../bin/generateIconPack')
+const path = require('path')
+
 module.exports = {
 	addons: [
 		'@storybook/addon-knobs',
@@ -11,7 +15,7 @@ module.exports = {
 		},
 	],
 	stories: ['../README.story.mdx', '../docs/**/*.story.mdx', '../src/**/*.story.jsx'],
-	webpackFinal: async (config) => {
+	webpackFinal: async (config, { configType }) => {
 		config.module.rules.push({
 			test: /\.jsx?$/,
 			loader: 'linaria/loader',
@@ -20,6 +24,18 @@ module.exports = {
 				cacheDirectory: 'src/.linaria_cache/storybook',
 			},
 		})
+
+		if (configType === 'PRODUCTION') {
+			// ICON hack for blueprintjs
+			extractIcons()
+
+			config.plugins.push(
+				new NormalModuleReplacementPlugin(
+					/.*\/generated\/iconSvgPaths.*/,
+					path.join(__dirname, '..', 'node_modules', '@blueprintjs', 'ICON_HACK.js')
+				)
+			)
+		}
 
 		return config
 	},
--- craco.config.js
+++ craco.config.js
@@ -1,12 +1,25 @@
 const CracoLinariaPlugin = require('craco-linaria')
-const { when, whenDev } = require('@craco/craco')
+const { when, whenDev, whenProd } = require('@craco/craco')
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 const StyleLintPlugin = require('stylelint-webpack-plugin')
+const { NormalModuleReplacementPlugin } = require('webpack')
+const { extractIcons } = require('./bin/generateIconPack')
 const path = require('path')
 
 module.exports = {
 	webpack: {
 		plugins: [
+			...whenProd(() => {
+				// ICON hack for blueprintjs
+				extractIcons()
+
+				return [
+					new NormalModuleReplacementPlugin(
+						/.*\/generated\/iconSvgPaths.*/,
+						path.join(__dirname, 'node_modules', '@blueprintjs', 'ICON_HACK.js')
+					),
+				]
+			}, []),
 			...when(Boolean(process.env.ANALYZE), () => [new BundleAnalyzerPlugin()], []),
 			...whenDev(
 				() => [
