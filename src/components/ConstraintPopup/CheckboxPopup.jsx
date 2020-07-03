6a7af51872ba72006a5c01eb689d695204d1013c
John Mendez <john.manuel.mendez@gmail.com>
2020-07-03 17:38:58 -0400

    Create a Checkbox type state machine

--- src/components/ConstraintPopup/CheckboxPopup.jsx
+++ src/components/ConstraintPopup/CheckboxPopup.jsx
@@ -1,7 +1,11 @@
 import { Checkbox, Label, NonIdealState } from '@blueprintjs/core'
 import { IconNames } from '@blueprintjs/icons'
+import { assign } from '@xstate/immer'
 import PropTypes from 'prop-types'
 import React from 'react'
+import { Machine } from 'xstate'
+
+import { LOCK_ALL_CONSTRAINTS, RECEIVE_SUMMARY, RESET_ALL_CONSTRAINTS } from '../../globalActions'
 
 export const CheckBoxPopup = ({
 	constraintChangeHandler = (_value) => (_e) => {},
@@ -37,3 +41,84 @@ CheckBoxPopup.propTypes = {
 	checkAll: PropTypes.bool,
 	disableAll: PropTypes.bool,
 }
+
+const ADD_CONSTRAINT = 'checkbox/constraint/add'
+const REMOVE_CONSTRAINT = 'checkbox/constraint/remove'
+const APPLY_CONSTRAINT = 'checkbox/constraint/apply'
+const RESET_CONSTRAINT = 'checkbox/constraint/reset'
+
+export const checkboxMachine = Machine(
+	{
+		id: 'CheckboxMachine',
+		initial: 'noConstraintsSet',
+		context: {
+			selectedValues: [],
+			availableValues: [],
+		},
+		on: {
+			[LOCK_ALL_CONSTRAINTS]: 'constraintLimitReached',
+			[RESET_ALL_CONSTRAINTS]: { target: 'noConstraintsSet', actions: 'removeAll' },
+			[RESET_CONSTRAINT]: { target: 'noConstraintsSet', actions: 'removeAll' },
+			[RECEIVE_SUMMARY]: { target: 'noConstraintsSet', actions: 'setAvailableValues' },
+		},
+		states: {
+			noConstraintsSet: {
+				on: {
+					[ADD_CONSTRAINT]: {
+						target: 'constraintsUpdated',
+						actions: 'addConstraint',
+					},
+				},
+			},
+			constraintsUpdated: {
+				always: [{ target: 'noConstraintsSet', cond: 'constraintListIsEmpty' }],
+				on: {
+					[ADD_CONSTRAINT]: { actions: 'addConstraint' },
+					[REMOVE_CONSTRAINT]: { actions: 'removeConstraint' },
+					[APPLY_CONSTRAINT]: 'constraintsApplied',
+				},
+			},
+			constraintsApplied: {
+				on: {
+					[ADD_CONSTRAINT]: {
+						target: 'constraintsUpdated',
+						actions: 'addConstraint',
+					},
+					[REMOVE_CONSTRAINT]: {
+						target: 'constraintsUpdated',
+						actions: 'removeConstraint',
+					},
+				},
+			},
+			constraintLimitReached: {
+				on: {
+					[REMOVE_CONSTRAINT]: { actions: 'removeConstraint' },
+				},
+			},
+		},
+	},
+	{
+		actions: {
+			// @ts-ignore
+			addConstraint: assign((ctx, { constraint }) => {
+				ctx.selectedValues.push(constraint)
+			}),
+			// @ts-ignore
+			removeConstraint: assign((ctx, { constraint }) => {
+				ctx.selectedValues = ctx.selectedValues.filter((cs) => cs.value !== constraint.value)
+			}),
+			removeAll: assign((ctx) => {
+				ctx.selectedValues = []
+			}),
+			setAvailableValues: assign((ctx, event) => {
+				// @ts-ignore
+				ctx.availableValues = event.summary.results
+			}),
+		},
+		guards: {
+			constraintListIsEmpty: (ctx) => {
+				return ctx.selectedValues.length === 0
+			},
+		},
+	}
+)
--- src/components/ConstraintPopup/CheckboxPopup.stories.jsx
+++ src/components/ConstraintPopup/CheckboxPopup.stories.jsx
@@ -1,85 +1,78 @@
 import { decorate } from '@storybook/addon-actions'
-import React, { useEffect, useState } from 'react'
-import { interpret } from 'xstate'
+import React, { useEffect } from 'react'
 
-import {
-	ADD_ORGANISM_CONSTRAINT,
-	APPLY_ORGANISM_CONSTRAINT,
-	RECEIVE_SUMMARY,
-	REMOVE_ALL_ORGANISM_CONSTRAINTS,
-	REMOVE_ORGANISM_CONSTRAINT,
-} from '../../actionConstants'
+import { RECEIVE_SUMMARY } from '../../actionConstants'
 import { useMachineBus } from '../../machineBus'
 import { organismSummary } from '../../stubs/geneSummaries'
 import { popupDecorator } from '../../utils/storybook'
 import { ConstraintPopup } from '../Constraints/ConstraintBase'
-import { organismMachine, OrganismPopup } from '../Constraints/Organism'
+import { checkboxMachineFactory, CheckBoxPopup } from './CheckboxPopup'
 
 export default {
 	title: 'Components/Popup Cards/CheckBox',
 	decorators: [...popupDecorator],
 }
 
-export const ConstraintNotSet = () => (
-	<div css={{ maxWidth: 500, minWidth: 376 }}>
-		<ConstraintPopup>
-			<OrganismPopup selectedOrganisms={[]} organisms={organismSummary.results} />
-		</ConstraintPopup>
-	</div>
-)
+// export const ConstraintNotSet = () => (
+// 	<div css={{ maxWidth: 500, minWidth: 376 }}>
+// 		<ConstraintPopup>
+// 			<OrganismPopup selectedOrganisms={[]} organisms={organismSummary.results} />
+// 		</ConstraintPopup>
+// 	</div>
+// )
 
-export const ConstraintsChanged = () => (
-	<div css={{ maxWidth: 500, minWidth: 376 }}>
-		<ConstraintPopup addEnabled={true} constraintSet={false}>
-			<OrganismPopup
-				selectedOrganisms={organismSummary.results.map((i) => ({ value: i.item }))}
-				organisms={organismSummary.results}
-			/>
-		</ConstraintPopup>
-	</div>
-)
+// export const ConstraintsChanged = () => (
+// 	<div css={{ maxWidth: 500, minWidth: 376 }}>
+// 		<ConstraintPopup addEnabled={true} constraintSet={false}>
+// 			<OrganismPopup
+// 				selectedOrganisms={organismSummary.results.map((i) => ({ value: i.item }))}
+// 				organisms={organismSummary.results}
+// 			/>
+// 		</ConstraintPopup>
+// 	</div>
+// )
 
-export const ConstraintsApplied = () => (
-	<div css={{ maxWidth: 500, minWidth: 376 }}>
-		<ConstraintPopup removeEnabled={true} constraintSet={true}>
-			<OrganismPopup
-				selectedOrganisms={organismSummary.results.slice(0, 3).map((i) => ({ value: i.item }))}
-				organisms={organismSummary.results}
-			/>
-		</ConstraintPopup>
-	</div>
-)
+// export const ConstraintsApplied = () => (
+// 	<div css={{ maxWidth: 500, minWidth: 376 }}>
+// 		<ConstraintPopup removeEnabled={true} constraintSet={true}>
+// 			<OrganismPopup
+// 				selectedOrganisms={organismSummary.results.slice(0, 3).map((i) => ({ value: i.item }))}
+// 				organisms={organismSummary.results}
+// 			/>
+// 		</ConstraintPopup>
+// 	</div>
+// )
 
 // const service = interpret(organismMachine)
 
 const conArgs = decorate([(args) => args.map((a) => JSON.stringify(a))])
 
 export const Playground = () => {
-	// const [state, setState] = useState(undefined)
-	const [state, send] = useMachineBus(organismMachine)
+	const [state, send, service] = useMachineBus(checkboxMachineFactory())
 
 	useEffect(() => {
 		// @ts-ignore
 		send({ type: RECEIVE_SUMMARY, summary: organismSummary })
-	}, [])
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [service.id])
 
 	const constraintChangeHandler = (constraint) => (e) => {
 		if (e.target.checked) {
 			// @ts-ignore
-			send({ type: ADD_ORGANISM_CONSTRAINT, constraint })
+			// send({ type: ADD_ORGANISM_CONSTRAINT, constraint })
 			conArgs.action('ADD')(constraint)
 		} else {
 			// @ts-ignore
-			send({ type: REMOVE_ORGANISM_CONSTRAINT, constraint })
+			// send({ type: REMOVE_ORGANISM_CONSTRAINT, constraint })
 			conArgs.action('REMOVE')(constraint)
 		}
 	}
 
 	const handleSubmit = (type) => {
 		if (type === 'REMOVE_CLICKED') {
-			send(REMOVE_ALL_ORGANISM_CONSTRAINTS)
+			// send(REMOVE_ALL_ORGANISM_CONSTRAINTS)
 		} else {
-			send(APPLY_ORGANISM_CONSTRAINT)
+			// send(APPLY_ORGANISM_CONSTRAINT)
 		}
 	}
 
@@ -95,9 +88,9 @@ export const Playground = () => {
 				constraintSet={!disableAll && enableRemoved}
 				handleSubmit={handleSubmit}
 			>
-				<OrganismPopup
-					organisms={state?.context.availableOrganisms}
-					selectedOrganisms={state?.context.selectedOrganisms}
+				<CheckBoxPopup
+					availableValues={state?.context.availableValues}
+					selectedValues={state?.context.selectedValues}
 					constraintChangeHandler={constraintChangeHandler}
 				/>
 			</ConstraintPopup>
